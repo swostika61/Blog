efddbd0081314d23edbf894201381b877f9e36fb
SumanMarahatha7 <sumancr.marahatha@gmail.com>
2022-03-30 15:00:38 +0545

    fixed props issues

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-const Card: React.FC<IPost> = ({data}) => {
-  console.log("1");
+import React from 'react'
+
+export interface IPostProps{
+  posts: IPost;
+}
+
+const Card:React.FC<IPostProps> = ({posts}) => {
   return (
     <div className="max-w-md sm:max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{data.id}</div>
-        <p className="text-gray-700 text-base">
-          {data.body}
-        </p>
-      </div>
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2">{posts.id}</div>
+      <p className="text-gray-700 text-base">
+        {posts.title}
+      </p>
     </div>
-  );
-};
+  </div>
+  )
+}
 
 export default Card;
